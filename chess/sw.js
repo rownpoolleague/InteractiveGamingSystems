1e05bced9541b4c40c9db411ef32cc0f4456a786
rownpoolleague <rownpoolleague@gmail.com>
2026-06-02 16:12:52 -0500

    Update sw.js

--- chess/sw.js
+++ chess/sw.js
@@ -1,5 +1,6 @@
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
 
 
 firebase.initializeApp({
