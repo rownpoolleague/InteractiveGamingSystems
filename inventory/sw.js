293ff2676c030f420ece3a46385ba8dc522cf714
rownpoolleague <rownpoolleague@gmail.com>
2026-06-16 14:43:40 -0500

    Update service worker to version 2 and enhance caching

--- inventory/sw.js
+++ inventory/sw.js
@@ -1,4 +1,5 @@
-const cacheName = 'inventory-v1';
+const version = 'v2'; // INCREMENT THIS (v3, v4, etc.) whenever you update your code
+const cacheName = `inventory-${version}`;
 const assets = [
   './',
   './index.html',
@@ -6,9 +7,25 @@ const assets = [
 ];
 
 self.addEventListener('install', e => {
+  self.skipWaiting(); // Forces the waiting service worker to become the active one immediately
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      // Return cached version if found, otherwise fetch from network
+      return res || fetch(e.request);
+    })
+  );
 });
