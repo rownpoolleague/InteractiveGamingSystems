b404d9904f704dc00d0c927158778e803a8e221f
rownpoolleague <rownpoolleague@gmail.com>
2026-07-03 05:04:15 -0500

    Update APPSCRIPT_URL in APP_CONFIG

--- chess/asfa.js
+++ chess/asfa.js
@@ -1,3 +1,3 @@
 const APP_CONFIG = {
-    APPSCRIPT_URL: "https://script.google.com/macros/s/AKfycbzNe_1lCgGMECC9oUYkrwqxoGPPSfkHWfeR-Liv9mlOLZk8_sZdYxLixhOjx106mrldOA/exec"
+    APPSCRIPT_URL: "https://script.google.com/macros/s/AKfycbxkONW-ZrlLMBCrxzSfj2aqeIr_WC_-W6aT-6fLW5IXBl8CDDk_Mz4A2j5DBZXZVvkC2Q/exec"
 };
