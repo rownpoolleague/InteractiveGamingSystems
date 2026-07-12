2ef73b3314cdefc11b6ef72eb4a813d500e1a672
rownpoolleague <rownpoolleague@gmail.com>
2026-07-12 01:25:53 -0500

    Add scoresheetinfo.js with team player data

---
+++ pooldemo/scoresheetinfo.js
@@ -0,0 +1,12 @@
+const scoresheetinfo = {
+    "1 Bank Shot Bandits": ["Aiden Miller", "Benjamin Scott", "Caleb Reed", "Declan Hayes", "Ethan Foster", "Finn Callahan", "Gavin Brooks"],
+    "2 Corner Pocket Kings": ["Henry Bennett", "Isaac Wright", "Jack Harrison", "Kieran Dawson", "Liam Sullivan", "Mason Clarke", "Noah Sullivan", "Owen Fletcher"],
+    "3 English Legends": ["Parker James", "Quinn Taylor", "Riley Cooper", "Samuel Evans", "Thomas Grant", "Ulysses Reed"],
+    "4 Pocket Pounders": ["Victor Nash", "Wyatt Stone", "Xavier Price", "Yosef Miller", "Zachary Boyd", "Adam West", "Brian Ross", "Charlie Day"],
+    "5 Chalk Dust Devils": ["David Lee", "Edward Kim", "Frank Hall", "George Hill", "Harry Kent", "Ian Long", "James Moss", "Kevin Nash"],
+    "6 Spin City Crew": ["Leo Owen", "Mike Page", "Nick Quinn", "Oscar Reed", "Paul Smith", "Quentin Tate", "Ryan Underhill", "Steve Vance"],
+    "7 Velocity Vipers": ["Tom White", "Umar Xander", "Victor Young", "Walter Zane", "Xavier Adams", "Yuri Baker", "Zachary Cole"],
+    "8 Felt Rippers": ["Aaron Davis", "Blake Evans", "Cody Fisher", "Dale Green", "Evan Harris", "Felix Irons"],
+    "9 Apex Predators": ["Grant Jones", "Henry King", "Isaac Lane", "Jack Miller", "Kyle Nash", "Liam Owen", "Miles Price"],
+    "10 Table Titans": ["Noah Quinn", "Owen Ross", "Paul Scott", "Quinn Tate", "Riley Underhill", "Samuel Vance", "Thomas White", "Ulysses Xander", "Victor Young"]
+};
