81098145ff006e0e309077c9eb1771c175e6a61e
rownpoolleague <rownpoolleague@gmail.com>
2026-08-22 02:31:41 -0500

    Increment service worker version to v4

--- inventory/sw.js
+++ inventory/sw.js
@@ -1,4 +1,4 @@
-const version = 'v3'; // INCREMENT THIS (v3, v4, etc.) whenever you update your code
+const version = 'v4'; // INCREMENT THIS (v3, v4, etc.) whenever you update your code
 const cacheName = `inventory-${version}`;
 const assets = [
   './',
